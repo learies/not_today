6799a3f8ec2a4819ded2dfb7fa4e438f69fc7b66
LeAries <learies@ya.ru>
2023-04-25 14:24:30 +0300

    feat: Добавлен api в config urls

--- backend/apps/products/api/urls.py
+++ backend/apps/products/api/urls.py
@@ -9,5 +9,5 @@ router_v1 = SimpleRouter()
 router_v1.register('сategories', CategoryViewSet, basename='сategories')
 
 urlpatterns = [
-    path('', include(router_v1.urls)),
+    path('v1/', include(router_v1.urls)),
 ]
